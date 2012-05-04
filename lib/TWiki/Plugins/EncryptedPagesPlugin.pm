9905643cda2517676711ab2616967a82c30c7472
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/EncryptedPagesPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/EncryptedPagesPlugin.pm
+++ lib/TWiki/Plugins/EncryptedPagesPlugin.pm
@@ -29,14 +29,14 @@
 #
 # =========================
 package TWiki::Plugins::EncryptedPagesPlugin;
+
 # =========================
 
 use vars qw(
-        $web $topic $user $installWeb  $VERSION $RELEASE $debug
-        $prefixPattern $postfixPattern
-        $replacementText
-    );
-
+  $web $topic $user $installWeb  $VERSION $RELEASE $debug
+  $prefixPattern $postfixPattern
+  $replacementText
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -48,52 +48,55 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-# 'Use locale' for internationalisation of Perl sorting and searching - 
+# 'Use locale' for internationalisation of Perl sorting and searching -
 # main locale settings are done in TWiki::setupLocale
 BEGIN {
+
     # Do a dynamic 'use locale' for this module
-    if( $TWiki::useLocale ) {
+    if ($TWiki::useLocale) {
         require locale;
-	import locale ();
+        import locale();
     }
 }
 
 # =========================
 # Plugin startup - read preferences and get all EncryptedPages Site->URL mappings
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between EncryptedPagesPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between EncryptedPagesPlugin and Plugins.pm");
         return 0;
     }
 
     # Get plugin preferences from EncryptedPagesPlugin topic
 
-    $debug = &TWiki::Func::getPreferencesFlag( "ENCRYPTEDPAGESPLUGIN_DEBUG" );
+    $debug = &TWiki::Func::getPreferencesFlag("ENCRYPTEDPAGESPLUGIN_DEBUG");
 
-    $prefixPattern  = '%ENCRYPTEDPAGE{';
-    $postfixPattern = '}%';
-    $replacementText = &TWiki::Func::getPreferencesValue("ENCRYPTEDPAGESPLUGIN_APPLET_INVOCATION");
+    $prefixPattern   = '%ENCRYPTEDPAGE{';
+    $postfixPattern  = '}%';
+    $replacementText = &TWiki::Func::getPreferencesValue(
+        "ENCRYPTEDPAGESPLUGIN_APPLET_INVOCATION");
 
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::EncryptedPagesPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    &TWiki::Func::writeDebug(
+"- TWiki::Plugins::EncryptedPagesPlugin::initPlugin( $web.$topic ) is OK"
+    ) if $debug;
     return 1;
 }
 
 # =========================
 # Expand the Site:page references, called once per line of text
-sub outsidePREHandler
-{
+sub outsidePREHandler {
 ### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
 
-    $replacementText = &TWiki::Func::expandCommonVariables( $replacementText, $topic, $web);
+    $replacementText =
+      &TWiki::Func::expandCommonVariables( $replacementText, $topic, $web );
     $_[0] =~ s/$prefixPattern(.*?)$postfixPattern/$replacementText/geo;
-    my $key= $1 || '';
+    my $key = $1 || '';
     $_[0] =~ s/--!!--/$key/geo;
 }
-
 
 1;
